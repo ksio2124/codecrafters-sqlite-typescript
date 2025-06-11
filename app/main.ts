218a897536f98ee139cad5321c761f7c4884fe02
jack <ksio2124@protonmail.ch>
2025-06-11 16:58:30 -0400

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -14,8 +14,8 @@ if (command === ".dbinfo") {
     console.error("Logs from your program will appear here!");
 
     // Uncomment this to pass the first stage    
-    // const pageSize = new DataView(buffer.buffer, 0, buffer.byteLength).getUint16(16);
-    // console.log(`database page size: ${pageSize}`);
+    const pageSize = new DataView(buffer.buffer, 0, buffer.byteLength).getUint16(16);
+    console.log(`database page size: ${pageSize}`);
 
     await databaseFileHandler.close();
 } else {
