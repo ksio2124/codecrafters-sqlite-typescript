2d74db5f9959adda08b300fae863137b6d125d2e
jack <ksio2124@protonmail.ch>
2025-06-15 17:29:22 -0400

    codecrafters submit [skip ci]

--- app/Row.ts
+++ app/Row.ts
@@ -40,4 +40,6 @@ export class Row {
     }
     return cursor;
   }
+
+  
 }
--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,17 @@ const args = process.argv;
 const databaseFilePath: string = args[2];
 const command: string = args[3];
 
+
+function trimMatchingQuotes(str: string) {
+  if (
+    (str.startsWith('"') && str.endsWith('"')) ||
+    (str.startsWith("'") && str.endsWith("'"))
+  ) {
+    return str.slice(1, -1);
+  }
+  return str;
+}
+
 const database = new Database(databaseFilePath);
 await database.init();
 await database.findAllTables();
@@ -19,17 +30,25 @@ if (parsed[0] === ".dbinfo") {
   const tableName = parsed.at(-1);
   const table = database.tables.filter((table) => table.name === tableName)[0];
   console.log(await table.getNumberOfRows());
-} else if (parsed[0].toUpperCase() === "SELECT" && parsed.at(-2)?.toUpperCase() === "FROM") {
-  const regex = /SELECT\s+(.+?)\s+FROM\s+(\w+)/i;
+} else if (parsed[0].toUpperCase() === "SELECT") {
+  const regex = /^SELECT\s+(.*?)\s+FROM\s+(\w+)(?:\s+WHERE\s+(.*))?$/i
+  // const regex = /SELECT\s+(.+?)\s+FROM\s+(\w+)/i
   const match = command.match(regex);
   const columns = (match?.[1] ?? '').split(',').map(s => s.trim());
   const tableName = match?.[2];
+  const [filterkey, filterVal] = (match?.[3] ?? '').split('=').map((val) => trimMatchingQuotes(val.trim())) ?? [];
+  // console.log(filterkey, filterVal)
   const tables = database.tables.filter(table => table.name === tableName);
   const rows = await tables[0].getAllRows();
 
   rows.forEach(row => {
     const output: string[] = [];
-    columns.forEach(colName => output.push(`${row.content[colName]}`))
+    if (row.content[filterkey] !== filterVal) {
+      return;
+    }
+    columns.forEach(colName => {
+      output.push(`${row.content[colName]}`)
+    })
     console.log(output.join('|'))
   });
 }else {
